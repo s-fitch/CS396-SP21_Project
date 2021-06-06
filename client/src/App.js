02cd8d4f557af18a7f3ace0bb2a596f1d9e106d6
s-fitch <SpencerFitch2022@u.northwestern.edu>
2021-06-05 23:03:20 -0500

    Header skeleton done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header';
+import Body from './Body';
+
 class App extends React.Component {
   render () {
     return (
-      <div>Hello World!</div>
+      <div style={{height: '100vh'}}>
+        <Header />
+        <Body />
+      </div>
     )
   }
 }
---
+++ client/src/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+class Body extends React.Component {
+  render () {
+    return (
+      <div className="container-fluid height-90 bg-info">
+          hi
+      </div>
+    )
+  }
+}
+
+export default Body;
