8a49cdc3ebdc68e5293f696c542b714c26fe172d
s-fitch <SpencerFitch2022@u.northwestern.edu>
2021-06-07 02:37:12 -0500

    Body renders based on login state

--- client/src/Body.js
+++ client/src/Body.js
@@ -2,20 +2,36 @@ import React from 'react';
 import './App.css';
 
 import CommunityList from './CommunityList';
+import CommunityView from './CommunityView';
 
 class Body extends React.Component {
   render () {
+    let content;
+    if (this.props.tokens) {
+        // Logged in
+        content = (
+            <div className="row" style={{flexWrap:"nowrap", height: "100%"}}>
+                <div className="col" style={{minWidth: "200px", padding: "0px", maxWidth:"200px"}}>
+                    <CommunityList 
+                        tokens={this.props.tokens}/>
+                </div>
+                <CommunityView />
+            </div>
+        )
+    } else {
+        content = (
+            <div className="row" style={{flexWrap:"nowrap", height: "100%"}}>
+                <div>
+                    Log in
+                </div>
+            </div>
+        )
+    }
+
     return (
-      <div className="container-fluid bg-success" style={{height: "90vh"}}>
-          <div className="row" style={{flexWrap:"nowrap", height: "100%"}}>
-              <div className="col" style={{minWidth: "200px", padding: "0px", maxWidth:"200px"}}>
-                  <CommunityList />
-              </div>
-              <div className="col">
-                  Col2
-              </div>
-          </div>
-      </div>
+        <div className="container-fluid bg-success" style={{height: "90vh"}}>
+            {content}
+        </div>
     )
   }
 }
