16157b62f857902406574d2b6c99fbec094cad7a
s-fitch <SpencerFitch2022@u.northwestern.edu>
2021-06-07 02:37:27 -0500

    Implemented basic button display

--- client/src/CommunityList.js
+++ client/src/CommunityList.js
@@ -1,14 +1,48 @@
 import React from 'react';
 
 class CommunityList extends React.Component {
-  render () {
-    const comms = Array.from(Array(100).keys()).map(i => <a className="row" key={i}>Community {i}</a>);
-    return (
-        <div className="container-fluid bg-white" style={{height: '100%', overflowY: "auto"}}>
-            {comms}
-        </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            communities: []
+        }
+    }
+
+    render () {
+
+        const communityList = this.state.communities.map(comm => (
+            <button type="button" className="btn btn-light btn-block" key={comm._id}>{comm.name}</button>
+        ))
+
+        return (
+            <div className="container-fluid bg-white" style={{height: '100%', overflowY: "auto"}}>
+                {communityList}
+            </div>
+        )
+    }
+
+    componentDidMount () {
+        if (this.props.tokens) {
+            fetch('/account/feed', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${this.props.tokens.access_token}`
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.message) {
+                    console.log(data.message);
+                    return;
+                } else {
+                    this.setState({
+                        communities: data
+                    })
+                }
+            })
+        }
+    }
 }
 
 export default CommunityList;
