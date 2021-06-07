3373ff48f83eab573306f49cb309e3e44a583e00
s-fitch <SpencerFitch2022@u.northwestern.edu>
2021-06-07 02:36:15 -0500

    Implemented login-logout modality

--- client/src/Account.js
+++ client/src/Account.js
@@ -3,9 +3,19 @@ import React from 'react';
 class Account extends React.Component {
 
     render () {
+        let buttonFunc;
+        let buttonText;
+
+        if (this.props.tokens) {
+            buttonFunc = this.props.logout;
+            buttonText = 'Logout';
+        } else {
+            buttonFunc = this.props.showLogin;
+            buttonText = 'Log In';
+        }
         return (
         <div className="d-flex">
-            <button className="btn btn-primary" type="button" onClick={this.props.showLogin}>Log In</button>
+            <button className="btn btn-primary" type="button" onClick={buttonFunc}>{buttonText}</button>
         </div>
         );
     }
