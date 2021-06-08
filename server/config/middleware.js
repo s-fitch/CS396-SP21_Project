3d70a82a9a61650d4df79205ecdae0afd80d91f4
s-fitch <SpencerFitch2022@u.northwestern.edu>
2021-06-07 23:58:05 -0500

    Updated CORS to allow for Authorization header

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,7 +1,7 @@
 module.exports = {
     cors: (_req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
         next();
     }
