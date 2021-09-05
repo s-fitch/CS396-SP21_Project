dde157f93e0bd3fa3bf8fc06400b9d4e2f5fd0dc
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-04 21:02:49 -0500

    Moved CommunityForm to its own file

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import CommunityForm from './CommunityForm';
 import CommunitySearch from './CommunitySearch';
 
 class Sidebar extends React.Component {
@@ -89,111 +89,5 @@ class Sidebar extends React.Component {
   }
 
 }
-
-
-class CommunityForm extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      communityTitle: '',
-      communityContent: ''
-    }
-
-    this.handleChange = this.handleChange.bind(this);
-    this.submitAnswer = this.submitAnswer.bind(this);
-  }
-
-  render() {
-    if (!this.props.show) {
-      return null;
-    }
-
-    return (
-      <form className="container-fluid card" style={{width: "100%"}}>
-        <div className="mb-3">
-          <input
-            type="text"
-            id="communityTitle"
-            className="form-control"
-            maxLength={35}
-            placeholder="Community name..."
-            onChange={this.handleChange}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <textarea 
-            className="form-control" 
-            rows="3" 
-            placeholder="Description..."
-            id="communityContent"
-            onChange={this.handleChange}
-          />
-        </div>
-        <button 
-          type="button"
-          className="btn btn-primary mb-1"
-          style={{width: "100%"}}
-          onClick={this.submitAnswer}
-        >
-          Submit
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-secondary"
-          style={{width: "100%"}}
-          onClick={this.props.close}
-        >
-          Cancel
-        </button>
-      </form>
-    );
-  }
-
-  handleChange(ev) {
-    this.setState({
-      [ev.target.id]: ev.target.value
-    })
-  }
-
-  submitAnswer() {
-    if (this.state.communityTitle === "" || this.state.communityContent === "") {
-      return;
-    }
-
-    const community = {
-      name: this.state.communityTitle,
-      description: this.state.communityContent
-    }
-
-    fetch(
-      `${process.env.REACT_APP_BACKEND_URL}/c/`, 
-      {
-        method: 'POST',
-        headers: {
-          'Authorization': `Bearer ${this.props.tokens.access_token}`,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(community)
-      })
-        .then(response => response.json())
-        .then(data => {
-          if (data.message) {
-            console.log(data);
-            return;
-          }
-          document.querySelector('#communityTitle').value = '';
-          document.querySelector('#communityContent').value = '';
-          this.setState({
-            communityTitle: '',
-            communityContent: ''
-          })
-          this.props.finished();
-        });
-  }
-
-}
-
 
 export default Sidebar;
