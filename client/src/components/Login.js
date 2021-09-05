85f3b2187ca1a28ff8daf5908ad53fbf143be234
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-04 20:06:13 -0500

    Combined Login and LoginForm into Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,7 +3,22 @@ import Logo from './Logo';
 import '../styles/Login.css';
 
 class Login extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signUp: false,
+      email: '',
+      password: ''
+    }
+
+    this.toggleSignUp = this.toggleSignUp.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.signUp = this.signUp.bind(this);
+  }
+
+  render() {
     return (
       <div style={{height: "100vh"}}>
         <nav className="navbar navbar-light bg-white" style={{height: "10vh"}}>
@@ -13,235 +28,196 @@ class Login extends React.Component {
           </div>
         </nav>
         <div style={{height: "90vh"}}>
-          <LoginForm 
-            finish={this.props.finish}/>
-          </div>
+          <form >
+            <h2 className="h2">
+              <b>{(this.state.signUp) ? "Sign Up" : "Log In"}</b>
+            </h2>
+            <div className="mb-3">
+              <div className="contianer-fluid d-flex justify-content-between">
+                <label htmlFor="email" className="form-label align-self-center" >
+                  Email address
+                </label>
+                <span className="align-self-center">
+                  {(this.state.signUp) ? "Already have an account?" : "Need an account?"}
+                  <a href="#" onClick={this.toggleSignUp}>
+                    {(this.state.signUp) ? "Log In" : "Sign Up"}
+                  </a>
+                </span>
+              </div>
+              <input 
+                type="email" 
+                id="email"
+                className="form-control" 
+                aria-describedby="emailHelp" 
+                placeholder="name@example.com" 
+                onChange={this.handleChange}
+                required
+              />
+              <div id="emailFeedback"></div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input 
+                type="password"
+                id="password" 
+                className="form-control" 
+                onChange={this.handleChange}
+                required
+              />
+              <div id="passwordFeedback"></div>
+            </div>
+            <div id="generalFeedback" className="alert alert-danger" style={{display: "none", padding: "10px"}}></div>
+            <button 
+              type="button" 
+              className="btn btn-primary" 
+              onClick={(this.state.signUp) ? this.signUp : this.logIn}>
+                {(this.state.signUp) ? "Sign Up" : "Log In"}
+            </button>
+          </form>
         </div>
-      )
-    }
-
-}
-
-class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.showLogIn = this.showLogIn.bind(this);
-    this.showSignUp = this.showSignUp.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleLogin = this.handleLogin.bind(this);
-    this.handleSignUp = this.handleSignUp.bind(this);
-    this.makePost = this.makePost.bind(this);
-    this.resetForm = this.resetForm.bind(this);
-
-    this.state = {
-      email: '',
-      password: '',
-
-      currText: 'Log in',
-      switchPrompt: 'Need an account?',
-      switchText: 'Sign Up',
-      handleSubmit: this.handleLogin,
-      handleSwitch: this.showSignUp
-    }    
-  }
-
-  render() {
-    return (
-      <form >
-        <h2 className="h2"><b>{this.state.currText}</b></h2>
-        <div className="mb-3">
-          <div className="contianer-fluid d-flex justify-content-between">
-            <label htmlFor="exampleInputEmail1" className="form-label align-self-center" >
-              Email address
-            </label>
-            <span className="align-self-center">
-              {this.state.switchPrompt}
-              <a href="#" onClick={this.state.handleSwitch}>
-                {this.state.switchText}
-              </a>
-            </span>
-          </div>
-          <input 
-            type="email" 
-            id="email"
-            className="form-control" 
-            aria-describedby="emailHelp" 
-            placeholder="name@example.com" 
-            onChange={this.handleChange}
-            required
-          />
-          <div id="emailFeedback"></div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input 
-            type="password"
-            id="password" 
-            className="form-control" 
-            onChange={this.handleChange}
-            required
-          />
-          <div id="passwordFeedback"></div>
-        </div>
-        <div id="generalFeedback" className="alert alert-danger" style={{display: "none", padding: "10px"}}></div>
-        <button type="button" className="btn btn-primary" onClick={this.state.handleSubmit}>
-          {this.state.currText}
-        </button>
-      </form>
+      </div>
     )
   }
 
-  handleChange (ev) {
+  toggleSignUp() {
+    const currState = this.state.signUp;
+    document.querySelector('#password').value = "";
+    this.setState({
+      password: "",
+      signUp: !currState
+    })
+  }
+
+  handleChange(ev) {
     this.setState({
       [ev.target.type]: ev.target.value
     })
   }
 
-  resetForm() {
-    this.validEmail();
-    this.validPassword();
-    this.feedbackSuccess();
-    document.querySelector('#email').value="";
-    document.querySelector('#password').value="";
-  }
-  
-  showLogIn () {
-    this.resetForm();
-    this.setState({
-      email: '',
-      password: '',
-
-      currText: 'Log in',
-      switchPrompt: 'Need an account?',
-      switchText: 'Sign Up',
-      handleSubmit: this.handleLogin,
-      handleSwitch: this.showSignUp
-    })
-  }
-  
-  showSignUp () {
-    this.resetForm();
-    this.setState({
-      email: '',
-      password: '',
-
-      currText: 'Sign Up',
-      switchPrompt: 'Already have an account?',
-      switchText: 'Log in',
-      handleSubmit: this.handleSignUp,
-      handleSwitch: this.showLogIn
-    })
-  }
-
-  invalidEmail(message) {
+  feedbackEmail(message) {
+    /**
+     * Provide feedback to user about email entry
+     * 
+     * @param {String} message Feedback message on invalid input, or null on valid input
+     */
     const input = document.querySelector('#email');
-    input.classList.add('is-invalid');
     const feedback = document.querySelector('#emailFeedback');
-    feedback.classList.add('invalid-feedback');
-    feedback.innerHTML=message;
-  }
-  
-  validEmail() {
-    const input = document.querySelector('#email');
-    input.classList.remove('is-invalid');
-    const feedback = document.querySelector('#emailFeedback');
-    feedback.classList.remove('invalid-feedback');
-    feedback.innerHTML="";
-  }
-  
-  validateEmail () {
-    const email = this.state.email;
 
+    if (message) {
+      input.classList.add('is-invalid');
+      feedback.classList.add('invalid-feedback');
+      feedback.innerHTML=message;
+    } else {
+      input.classList.remove('is-invalid');
+      feedback.classList.remove('invalid-feedback');
+      feedback.innerHTML="";
+    }
+
+  }
+
+  feedbackPassword(message) {
+    /**
+     * Provide feedback to user about password entry
+     * 
+     * @param {String} message Feedback message on invalid input, or null on valid input
+     */
+    const input = document.querySelector('#password');
+    const feedback = document.querySelector('#passwordFeedback');
+
+    if (message) {
+      input.classList.add('is-invalid');
+      feedback.classList.add('invalid-feedback');
+      feedback.innerHTML=message;
+    } else {
+      input.classList.remove('is-invalid');
+      feedback.classList.remove('invalid-feedback');
+      feedback.innerHTML="";
+    }
+
+  }
+
+  feedbackPost(message) {
+    /**
+     * Provide feedback to user after attempting to Log In or Sign Up
+     * 
+     * @param {String} message Feedback message on invalid input, or null on valid input
+     */
+    const feedback = document.querySelector('#generalFeedback');
+
+    if (message) {
+      document.querySelector('#password').value = "";
+      feedback.style.display = "block";
+      feedback.innerHTML = message;
+    } else {
+      feedback.style.display= "none";
+      feedback.innerHTML = "";
+    }
+
+  }
+
+  validateEmail (email) {
+    /**
+     * Validate current email address entry by user
+     * 
+     * @param {string} email current email address entry
+     */
     if (email === "") {
-      this.invalidEmail("Please enter your email address");
+      this.feedbackEmail("Please enter your email address");
       return false;
     }
 
     const regEmail = new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$")
-      if (!regEmail.test(email)) {
-        console.log('ugh')
-        this.invalidEmail("Email address must conform to format: name@example.com");
-        return false;
-      }
+    if (!regEmail.test(email)) {
+      this.feedbackEmail("Email address must conform to format: name@example.com");
+      return false;
+    }
 
-    this.validEmail();
+    this.feedbackEmail(null);
     return true;
   }
 
-  invalidPassword(message) {
-    const input = document.querySelector('#password');
-    input.classList.add('is-invalid');
-    const feedback = document.querySelector('#passwordFeedback');
-    feedback.classList.add('invalid-feedback');
-    feedback.innerHTML=message;
-  }
-  
-  validPassword() {
-    const input = document.querySelector('#password');
-    input.classList.remove('is-invalid');
-    const feedback = document.querySelector('#passwordFeedback');
-    feedback.classList.remove('invalid-feedback');
-    feedback.innerHTML="";
-  }
-  
-  validatePassword() {
-    const password = this.state.password;
-
+  validatePassword(password) {
+    /**
+     * Validate current password entry by user
+     * 
+     * @param {string} password current password entry
+     */
     if (password === "") {
-      this.invalidPassword("Please enter your password");
+      this.feedbackPassword("Please enter your password");
       return false;
     }
 
     if (password.length > 20 || password.length < 8){
-      this.invalidPassword("Password must be 8-20 characters");
+      this.feedbackPassword("Password must be 8-20 characters");
       return false;
     }
 
-    this.validPassword();
+    this.feedbackPassword(null);
     return true;
   }
 
-  feedbackFail(message) {
-    document.querySelector('#password').value = "";
-    const feedback = document.querySelector('#generalFeedback');
-    feedback.style.display = "block";
-    feedback.innerHTML = message;
-    return;
-  }
-  
-  feedbackSuccess() {
-    const feedback = document.querySelector('#generalFeedback');
-    feedback.style.display= "none";
-    feedback.innerHTML = "";
-    return;
-  }
+  authPost(endpoint) {
+    /**
+     * Make a POST request to obtain authorization tokens with current email and password
+     * 
+     * @param {string} endpoint - API endpoint to make POST request to
+     */
+    const email = this.state.email;
+    const password = this.state.password;
 
-  handleLogin () {
-    this.makePost('/account/login');
-  }
-  
-  handleSignUp () {
-    this.makePost('/account');
-  }
-
-  makePost(path) {
-    const validEmail = this.validateEmail();
-    const validPassword = this.validatePassword();
-    if (!(validEmail && validPassword)) {
+    if (!this.validateEmail(email) || !this.validatePassword(password)) {
       return;
     }
 
     const reqBody = {
-      'email': this.state.email,
-      'password': this.state.password
+      'email': email,
+      'password': password,
     }
 
-    // Make Login attempt
     fetch(
-      `${process.env.REACT_APP_BACKEND_URL}${path}`, 
+      `${process.env.REACT_APP_BACKEND_URL}${endpoint}`, 
       {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -250,19 +226,26 @@ class LoginForm extends React.Component {
         .then(response => response.json())
         .then(data => {
           if (data.message) {
-            // Failed authentication
             console.log(data);
-            this.feedbackFail(data.message);
+            this.feedbackPost(data.message);
             return;
           }
 
-          this.feedbackSuccess();
+          this.feedbackPost(null);
           this.props.finish(data);
 
         })
         .catch(err => console.log(err));
   }
+
+  logIn () {
+    this.authPost('/account/login');
+  }
   
+  signUp () {
+    this.authPost('/account');
+  }
+
 }
 
 export default Login;
