4f67c9ba4a704e892620bb3e699786ac2523e781
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-04 20:47:14 -0500

    Simplified state management and added comments to custom methods

--- client/src/App.js
+++ client/src/App.js
@@ -7,29 +7,14 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		/**
-		 * State Parameters
-		 *  currView - Current view state
-		 *      0: Base App
-		 *      1: Log In/Sign Up
-		 *  prevView - Previous view state (for knowing where back returns to?)
-	   *      0: Base App
-		 *      1: Log In/Sign Up
-		 */
 		let tokens = this.getTokensFromCookie();
 		tokens = (Boolean(tokens.access_token)&&Boolean(tokens.refresh_token)) ? tokens : null;
 
 		this.state = {
-			currView: 0,
-			prevView: 0,
+			login: false,
 			tokens: tokens,
 			community: null
 		}
-
-		this.showLogin = this.showLogin.bind(this);
-		this.hideLogin = this.hideLogin.bind(this);
-		this.finishLogin = this.finishLogin.bind(this);
-		this.logout = this.logout.bind(this);
 
 		this.getCookie = this.getCookie.bind(this);
 		this.setCookie = this.setCookie.bind(this);
@@ -38,61 +23,76 @@ class App extends React.Component {
 		this.getTokensFromCookie = this.getTokensFromCookie.bind(this);
 		this.setTokensInCookie = this.setTokensInCookie.bind(this);
 		this.clearTokensFromCookie = this.clearTokensFromCookie.bind(this);
+
+		this.showLogin = this.showLogin.bind(this);
+		this.hideLogin = this.hideLogin.bind(this);
+		this.finishLogin = this.finishLogin.bind(this);
+		this.logout = this.logout.bind(this);
 		
 		this.selectCommunity = this.selectCommunity.bind(this);
-		this.showLandingPage = this.showLandingPage.bind(this);
+		this.showLanding = this.showLanding.bind(this);
 	}
 
 	render() {
-		switch (this.state.currView) {
-			case 0:
-				return (
-					<div style={{height: '100vh'}}>
-						<Header 
-							tokens={this.state.tokens}
-							logout={this.logout}
-							showLogin={this.showLogin}
-							showHome={this.showLandingPage}
-						/>
-						<Main 
-							tokens = {this.state.tokens}
-							community = {this.state.community}
-							selectCommunity = {this.selectCommunity}
-						/>
-					</div>
-				);
-			case 1:
-				return (
-					<Login 
-						hide={this.hideLogin}
-						finish={this.finishLogin}
+		return (
+			(this.state.login) ? (
+				<Login 
+					hide={this.hideLogin}
+					finish={this.finishLogin}
+				/>
+			) : (
+				<div style={{height: '100vh'}}>
+					<Header 
+						tokens={this.state.tokens}
+						logout={this.logout}
+						showLogin={this.showLogin}
+						showHome={this.showLanding}
 					/>
-				);
-			default:
-				return (
-					<div>
-						What did you do??
-					</div>
-				)
-		}
+					<Main 
+						tokens = {this.state.tokens}
+						community = {this.state.community}
+						selectCommunity = {this.selectCommunity}
+					/>
+				</div>
+			)
+		);
 	}
 
 	getCookie (key) {
+		/**
+		 * Retrieve specified cookie
+		 * 
+		 * @param {String} key identifier for desired cookie
+		 */
 		const cookie = document.cookie.split('; ').find(elem => elem.startsWith(`${key}=`))
 		return (cookie) ? cookie.split('=')[1] : null;
 	}
 	
 	setCookie (key, value) {
+		/**
+		 * Set value for specified cookie
+		 * 
+		 * @param {String} key identifier for desired cookie
+		 * @param {String} value value to store in cookie
+		 */
 		document.cookie = `${key}=${value}`;
 		return;
 	}
   
 	clearCookie (key) {
+		/**
+		 * Clear specified cookie
+		 * 
+		 * @param {String} key identifier for desired cookie
+		 */
 		document.cookie = `${key}= ; expires = Thu, 01 Jan 1970 00:00:00 GMT`
 		return;
 	}
 
 	getTokensFromCookie () {
+		/**
+		 * Retreive application JSON Web Tokens (JWT) from cookie
+		 */
 		return {
 			access_token: this.getCookie('access_token'),
 			refresh_token: this.getCookie('refresh_token')
@@ -100,60 +100,85 @@ class App extends React.Component {
 	}
   
 	setTokensInCookie(tokens) {
+		/**
+		 * Store application JSON Web Tokens (JWT) in cookie
+		 * 
+		 * @param {Object} tokens JWT to store
+		 */
 		this.setCookie('access_token', tokens.access_token);
 		this.setCookie('refresh_token', tokens.refresh_token);
 		return;
 	}
   
 	clearTokensFromCookie () {
+		/**
+		 * Clear application JSON Web Tokens (JWT) from cookie
+		 */
 		this.clearCookie('access_token');
 		this.clearCookie('refresh_token');
 	}
 
-	showLogin () {
+	showLogin() {
+		/**
+		 * Show account login page
+		 */
 		this.setState({
-			prevView: this.state.currView,
-			currView: 1
+			login: true
 		})
 	}
 
 	hideLogin () {
+		/**
+		 * Hide account login page
+		 */
 		this.setState({
-			currView: this.state.prevView
+			login: false
 		})
 	}
   
 	finishLogin(tokens) {
+		/**
+		 * Finish login process 
+		 */
 		this.setTokensInCookie(tokens);
 		this.setState({
-			currView: 0,
-			prevView: 0,
+			login: false,
 			tokens: tokens,
 			community: null
 		})
 	}
 	
 	logout () {
-		this.clearTokensFromCookie();
-		this.setState({
-			loggedIn: false,
-			tokens: null,
-			community: null
-		})
+		/**
+		 * Log user out of application
+		 */
+		if (window.confirm("Are you sure you want to logout?")) {
+			this.clearTokensFromCookie();
+			this.setState({
+				tokens: null,
+				community: null
+			})
+		}
 	}
     
 	selectCommunity (ev) {
+		/**
+		 * Select community to display
+		 */
 		this.setState({
 			community: ev.target.id
 		});
 	}
   
-	showLandingPage () {
+	showLanding () {
+		/**
+		 * Show application landing page
+		 */
 		this.setState({
 			community: null
 		})
 	}
-	
+
 }
 
 export default App;
